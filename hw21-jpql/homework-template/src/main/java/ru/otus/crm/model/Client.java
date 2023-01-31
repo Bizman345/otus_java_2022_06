6ae2a5f4567cb63c39284eb2073790b5a01e6fba
Сергей Бизюков <bizman345@gmail.com>
2023-01-31 23:36:19 +0300

    hw21-jpql Использование Hibernate

---
+++ hw21-jpql/homework-template/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,73 @@
+package ru.otus.crm.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phone;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+        this.address = null;
+        this.phone = new ArrayList<>();
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.address = null;
+        this.phone = new ArrayList<>();
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+
+        if (phone != null) {
+            this.phone = phone;
+            this.phone.forEach(ph -> ph.setClient(this));
+        } else {
+            this.phone = new ArrayList<>();
+        }
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name, this.address, this.phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
